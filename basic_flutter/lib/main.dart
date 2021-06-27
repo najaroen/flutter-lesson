0bb1e2613bca562f657256bcaf5cd93b6d169f9c
najaroendev <najaroen@ragnar.co.th>
2021-06-27 14:45:41 +0700

    add stateful.dart

--- basic_flutter/lib/main.dart
+++ basic_flutter/lib/main.dart
@@ -5,6 +5,10 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  void onClick() {
+    print("Hello guys");
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -17,7 +21,15 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text("Basic Flutter"),
         ),
-        body: Text("Body"),
+        body: Column(
+          children: [
+            Text("Hello1"),
+            Text("Hello2"),
+            // IconButton(onPressed: onPressed, icon: Icons.ac_unit_rounded)
+            RaisedButton(onPressed: onClick, child: Text("Click Here"),),
+            RaisedButton(onPressed: onClick, child: Text("Click Here"),)
+          ],
+        ),
       ),
     );
   }
